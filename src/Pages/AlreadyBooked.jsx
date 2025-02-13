9a9931aadada555d84e6440b262d3eed12edb7d8
M7mdU <mohamedyosef67@gmail.com>
2025-02-13 21:30:47 +0200

    test
    1

--- src/Pages/AlreadyBooked.jsx
+++ src/Pages/AlreadyBooked.jsx
@@ -5,6 +5,7 @@ export default function BookingSucced() {
 	// const navigate = useNavigate();
 	return (
 		<div className="card-box w-full lg:w-3/5 xl:w-2/5 max-w-7xl flex flex-col items-center">
+			<h1>Hi</h1>
 			<img src={fireworks} alt="celebrate icon" className="mx-auto mb-8" />
 			<div className="flex flex-col gap-y-4">
 				<h1 className="text-3xl font-semibold text-center">
